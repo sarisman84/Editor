77edc05d6746911195ecdf9b0a036600106e103d
Spyridon Passas <epicdudeness677@gmail.com>
2022-09-19 08:32:26 +0200

    Added Editor Project

---
+++ Source/Editor/premake5.lua
@@ -0,0 +1,95 @@
+-- require "audio"
+
+os.mkdir("../../Bin/Editor")
+-- audio.beep("square",4000,100)
+project "Editor"
+	dependson { "External" }
+
+	location "%{wks.location}/Local/"
+	kind "WindowedApp"
+	language "C++"
+	cppdialect "C++17"
+	--staticruntime "on"
+
+	targetdir ("../../Bin/")
+	targetname("%{prj.name}_%{cfg.buildcfg}")
+	objdir ("../../Temp/%{prj.name}/%{cfg.buildcfg}")
+
+	debugdir "../../Bin/"
+
+	files {
+		"**.h",
+		"**.cpp",
+	}
+
+	includedirs {
+		"../Engine",
+		"../Game/",
+        "../External/"
+	}
+
+	libdirs { "../../Lib/" }
+
+	links { 
+		"avcodec.lib",
+		"avdevice.lib",
+		"avfilter.lib",
+		"avformat.lib",
+		"avutil.lib",
+		"postproc.lib",
+		"swresample.lib",
+		"swscale.lib",
+		"Game",
+		"Engine"
+	}
+
+	defines {"_CONSOLE"}
+	
+	filter "configurations:Debug"
+		defines {"_DEBUG"}
+		runtime "Debug"
+		symbols "on"
+		
+		links {
+			"External_Debug",
+		}
+		
+	filter "configurations:Release"
+		defines "_RELEASE"
+		runtime "Release"
+		optimize "on"
+		
+		links {
+			"External_Release",
+		}
+
+	filter "configurations:Retail"
+		defines "_RETAIL"
+		runtime "Release"
+		optimize "on"
+		
+		links {
+			"External_Release",
+		}
+
+	systemversion "latest"
+	
+	filter "system:windows"
+		symbols "On"		
+		systemversion "latest"
+		warnings "Extra"
+		-- sdlchecks "true"
+		--conformanceMode "On"
+		--buildoptions { "/STACK:8000000" }
+		flags { 
+		--	"FatalWarnings", -- would be both compile and lib, the original didn't set lib
+			"FatalCompileWarnings",
+			"MultiProcessorCompile"
+		}
+		defines {
+			"WIN32",
+			"_LIB", 
+			"TGE_SYSTEM_WINDOWS"
+		}
+		
+		
